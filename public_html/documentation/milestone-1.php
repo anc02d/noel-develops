f9544bcf440b6237c0721318993bb205e5055f37
Noel Cothren <noelcothren@gmail.com>
2017-01-21 11:34:42 -0700

    added all milestone 1 written documentation

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+
+	</head>
+	<body>
+		<h1>Personal Website Project: Milestone 1 Documentation</h1>
+		<h2>Purpose</h2>
+		<p>A successful portfolio site that stands out from the crowd and achieves a perfect blend of personality, prominence of work, simplicity, and ease of use. It will show prospective employers what I can do and allow them to contact me.</p>
+		<h2>Audience</h2>
+		<p>Potential employers - including my peers and other freelancers, owners of small and large development businesses, and start-ups who need contracted or full- or part-time help.</p>
+		<h2>Goals</h2>
+		<ul>
+			<li>To acquire full or part-time employment.</li>
+			<li>To attract freelance clients and contract work.</li>
+			<li>To showcase my skills as a developer and promote my work.</li>
+		</ul>
+		<h2>User Persona</h2>
+		<p><strong>Name:</strong> Jimmy O. Wner </p>
+		<p><strong>Age:</strong> 34 </p>
+		<p><strong>Profession:</strong> Co-Founder and Owner of web-develop-all-day.com, a small web development business based in Albuquerque, NM that is looking to hire a full-time, remote Front-End Junior Web Developer. They currently employ 9 remote developers and are looking to hire one more junior developer that has the ability to tweak the front-end and UX of their products, as well as someone they can give simple problems and bug fix work to, in order to clear up their other developers' schedules for bigger issues. </p>
+		<p><strong>Technology:</strong> Macbook Pro Laptop running the latest OS (OSX Sierra as of this report), connected to a 30” cinema display. He also uses an iPhone 7 with 4G LTE data connection that is limited to 30gb/month. He uses Google Chrome web browser on his desktop and Safari on his iPhone. </p>
+		<p><strong>Attitudes and Behaviors:</strong> An easy-going guy, but serious about his company. Jimmy wants to hire someone with talent and will take his time to find the right person, since they are not under-staffed at the time. He keeps a list of potential candidate's sites and will look at portfolios on his laptop during his free breaks from work, or during lulls in the day from anywhere using his iPhone. He never spends more than 10 minutes on any particular portfolio site. Jimmy uses social media like Facebook and Twitter, but is not on LinkedIn. He is also a member of multiple professional groups and seeks potential candidates through community networking events.</p>
+		<p><strong>Frustrations and Needs:</strong> Jimmy needs to hire someone with  who is willing to start at a lower salary, but who has the skills to write professional code and learn on the job to become a more valuable employee. He is especially looking for someone with a good eye for aesthetics that can create a great User Experience. Even though he is looking for someone who has a good personality,  since all of their employees work from home the cultural fit isn't as important. Because web-develop-all-day.com isn't desperate for a new employee, Jimmy is taking his time to find a candidate with the right combination of talent, experience, and personality who is willing to accept a lower salary. He has met and reviewed many candidates in the past month, but hasn't yet found one he thinks will be a good investment.</p>
+		<p><strong>Goals:</strong>By browsing portfolio sites, Jimmy wants to find out if the person could be a candidate for their Junior Developer position. He wants to look at the UX and the source code of sites to determine the candidate's abilities and get a feel for their personality and work style. If he likes what he sees, he wants to be able to initiate contact via the site, and possibly even set up a meeting immediately.</p>
+		<h2>Use Case</h2>
+		<p>Jimmy and I met at a networking event, where I gave him a business card with my website URL on it. Since we seemed to hit it off and I mentioned my arts background, he decided to peruse my portfolio site.</p>
+		<p>He is currently eating dinner and decided to check out my site on his iPhone. He is somewhat distracted and tired from a full day's work.
+		</p>
+
+	</body>
+</html>
