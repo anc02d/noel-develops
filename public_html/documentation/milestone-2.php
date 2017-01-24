98b4533a50a88e9cb96378711b55b2e62347b1cb
Noel Cothren <noelcothren@gmail.com>
2017-01-23 19:07:47 -0700

    just some layout/placeholders for more content

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,7 +5,7 @@
 		<title>PWP Milestone 2</title>
 	</head>
 	<body>
-		<h1>Personal Website Project Milestone 2</h1>
+		<h1>Personal Website Project: Milestone 2 Documentation</h1>
 		<h2>Content Strategy</h2>
 		<p>
 			My content will be my development portfolio (hopefully filled with some interesting and useful projects), a list of the types of work I'm capable of and/or a skill set graph, my contact information, including my email, a business phone number, and links to linkedIn, Instagram, etc, and finally, some reviews from happy customers or employers saying how great of a person I am to work with. In addition to the links and contact info, there will be a contact form so viewers can directly reach me through the site. Other ideas include:</p>
@@ -16,5 +16,7 @@
 				<li>Photo of me</li>
 				<li>Downloadable resume</li>
 			</ul>
+		<h2>Wireframes</h2>
+		<p>Coming soon...</p>
 	</body>
 </html>
