60f0e9700afb5ee2836aa5ec4f4e47653246e33b
Noel Cothren <noelcothren@gmail.com>
2017-01-23 18:57:47 -0700

    basic html doc with a rough rough draft of my content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PWP Milestone 2</title>
+	</head>
+	<body>
+		<h1>Personal Website Project Milestone 2</h1>
+		<h2>Content Strategy</h2>
+		<p>
+			My content will be my development portfolio (hopefully filled with some interesting and useful projects), a list of the types of work I'm capable of and/or a skill set graph, my contact information, including my email, a business phone number, and links to linkedIn, instagram, etc, and finally, some reviews from happy customers or employers saying how great of a person I am to work with. In addition to the links and contact info, there will be a contact form so viewers can directly reach me through the site.
+		</p>
+	</body>
+</html>
