f0dc3bbd8c36e33fff5b22f30e5542e06a757999
Noel Cothren <noelcothren@gmail.com>
2017-01-23 19:45:46 -0700

    content edits

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
+
 		<meta charset="utf-8">
+
 		<title>PWP Milestone 2</title>
+
 	</head>
+
 	<body>
+
 		<h1>Personal Website Project: Milestone 2 Documentation</h1>
+
 		<h2>Content Strategy</h2>
+
 		<p>Since my purpose is to showcase my abilities as a Web Developer, the main content on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I will also list the software I'm comfortable with, the toolsets I have experience using, a visual representation of my skills in the form of a graph or chart, and maybe some certificates from Lynda, etc to show my dedication to learning.</p>
 
 		<p>Of course I'll need to include my contact information, including my email, a business phone number, links to my linkedIn and Instagram profiles, and an embedded contact form so potential employers can reach me directly through the site.</p>
@@ -21,7 +29,11 @@
 				<li>Photos or art projects I'm proud of</li>
 				<li>Links to more personal sites/project, and/or my blog</li>
 			</ul>
+
 		<h2>Wireframes</h2>
+
 		<p>Coming soon...</p>
+
 	</body>
+
 </html>
