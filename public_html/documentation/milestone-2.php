5b9110d4292fca2352539b8785c55a356a7d7822
Noel Cothren <noelcothren@gmail.com>
2017-01-23 19:27:28 -0700

    content edits

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,14 +7,19 @@
 	<body>
 		<h1>Personal Website Project: Milestone 2 Documentation</h1>
 		<h2>Content Strategy</h2>
-		<p>
-			My content will be my development portfolio (hopefully filled with some interesting and useful projects), a list of the types of work I'm capable of and/or a skill set graph, my contact information, including my email, a business phone number, and links to linkedIn, Instagram, etc, and finally, some reviews from happy customers or employers saying how great of a person I am to work with. In addition to the links and contact info, there will be a contact form so viewers can directly reach me through the site. Other ideas include:</p>
+		<p>Since my purpose is to showcase my abilities as a Web Developer, the main content on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I will also list the software I'm comfortable with, the toolsets I have experience using, a visual representation of my skills in the form of a graph or chart, and maybe some certificates from Lynda, etc to show my dedication to learning.</p>
+
+		<p>Of course I'll need to include my contact information, including my email, a business phone number, links to my linkedIn and Instagram profiles, and an embedded contact form so potential employers can reach me directly through the site.</p>
+
+		<p>Finally, I'd love to add some reviews from happy customers, co-workers, instructors, or employers saying how great of a person I am to work with so that hiring authorities on the fence might be swayed to get in touch.</p>
+
+		Other ideas I might include:
 			<ul>
+				<li>Downloadable resume</li>
+				<li>Photo of me</li>
 				<li>List of books I'm currently reading</li>
 				<li>Photos or art projects I'm proud of</li>
 				<li>Links to more personal sites/project, and/or my blog</li>
-				<li>Photo of me</li>
-				<li>Downloadable resume</li>
 			</ul>
 		<h2>Wireframes</h2>
 		<p>Coming soon...</p>
