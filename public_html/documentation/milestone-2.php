0d8f759c711709638a3dd775c81f830afb3b5923
Noel Cothren <noelcothren@gmail.com>
2017-01-23 19:02:03 -0700

    additional content

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,7 +8,13 @@
 		<h1>Personal Website Project Milestone 2</h1>
 		<h2>Content Strategy</h2>
 		<p>
-			My content will be my development portfolio (hopefully filled with some interesting and useful projects), a list of the types of work I'm capable of and/or a skill set graph, my contact information, including my email, a business phone number, and links to linkedIn, instagram, etc, and finally, some reviews from happy customers or employers saying how great of a person I am to work with. In addition to the links and contact info, there will be a contact form so viewers can directly reach me through the site.
-		</p>
+			My content will be my development portfolio (hopefully filled with some interesting and useful projects), a list of the types of work I'm capable of and/or a skill set graph, my contact information, including my email, a business phone number, and links to linkedIn, Instagram, etc, and finally, some reviews from happy customers or employers saying how great of a person I am to work with. In addition to the links and contact info, there will be a contact form so viewers can directly reach me through the site. Other ideas include:</p>
+			<ul>
+				<li>List of books I'm currently reading</li>
+				<li>Photos or art projects I'm proud of</li>
+				<li>Links to more personal sites/project, and/or my blog</li>
+				<li>Photo of me</li>
+				<li>Downloadable resume</li>
+			</ul>
 	</body>
 </html>
