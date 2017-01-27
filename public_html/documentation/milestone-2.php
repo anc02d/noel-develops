a13490ea2877e7b9651db80a574a0361926b31f7
Noel Cothren <noelcothren@gmail.com>
2017-01-27 15:01:43 -0700

    revamp content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,34 +1,22 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 	<head>
-
 		<meta charset="utf-8">
-
 		<title>PWP Milestone 2</title>
-
 	</head>
-
 	<body>
-
 		<h1>Personal Website Project: Milestone 2 Documentation</h1>
-
-		<h2>Content Strategy</h2>
-
-		<p>Since my purpose is to showcase my abilities as a Web Developer, the main content on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I will also list the software I'm comfortable with, the toolsets I have experience using, a visual representation of my skills in the form of a graph or chart, and maybe some certificates from Lynda, etc to show my dedication to learning.</p>
-
-		<p>Of course I'll need to include my contact information, including my email, a business phone number, links to my linkedIn and Instagram profiles, and an embedded contact form so potential employers can reach me directly through the site.</p>
-
-		<p>Finally, I'd love to add some reviews from happy customers, co-workers, instructors, or employers saying how great of a person I am to work with so that hiring authorities on the fence might be swayed to get in touch.</p>
-
-		Other ideas I might include:
-			<ul>
-				<li>Downloadable resume</li>
-				<li>Photo of me</li>
-				<li>List of books I'm currently reading</li>
-				<li>Photos or art projects I'm proud of</li>
-				<li>Links to more personal sites/project, and/or my blog</li>
-			</ul>
+			<h2>Content Strategy</h2>
+				<p>My site will be one static page, and will consist of the following content: </p>
+				<h3>First Scroll-point/Opening Page</h3>
+					<p>The first thing I want people to see is a well-thought-out (read: aesthetically pleasing and incredibly responsive) image frame with either my picture or a video (exact content tbd), overlaid with a "hello/welcome" message and my name. I would like it to be simple yet intriguing enough to make visitors scroll down. I am considering including a favorite quote.</p>
+				<h3>Second Scroll-point/Portfolio</h3>
+					<p>Since my purpose is to showcase my abilities as a Web Developer, the very next thing on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I think a gallery of images with clickable links will be ideal.</p>
+				<h3>Third Scroll-point/Skillz, yo</h3>
+					<p>Next, I will list the software, tool sets, and other skills I'm proficient in or have experience using. It's possible I'll use a visual representation of my skills in the form of a graph or chart, and maybe add links to some certificates from Lynda, etc to show my dedication to learning.</p>
+				<h3>Final Scroll-point/Contact Me!</h3>
+					<p>The next thing I'll need include will be my contact information, including my email address, links to my linkedIn and Instagram profiles, and a link to my downloadable resume. At the end of the page will be an embedded contact form so potential employers can reach me directly through the site.</p>
+					<p>I'm considering adding some reviews from happy customers, co-workers, instructors, or employers saying how great of a person I am to work with so that hiring authorities on the fence might be swayed to get in touch - we shall see what I come up with.</p>
 
 		<h2>Wireframes</h2>
 
