d22a28269a552a33951c142904e5b54f982b04f7
Noel Cothren <noelcothren@gmail.com>
2017-01-28 15:38:49 -0700

    Added an "about me" section

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,12 +12,12 @@
 					<p>The first thing I want people to see is a well-thought-out (read: aesthetically pleasing and incredibly responsive) image frame with either my picture or a video (exact content tbd), overlaid with a "hello/welcome" message and my name. I would like it to be simple yet intriguing enough to make visitors scroll down. I am considering including a favorite quote.</p>
 				<h3>Second Scroll-point/Portfolio</h3>
 					<p>Since my purpose is to showcase my abilities as a Web Developer, the very next thing on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I think a gallery of images with clickable links will be ideal.</p>
-				<h3>Third Scroll-point/Skillz, yo</h3>
+				<h3>Third Scroll-point/About Me</h3>
+					<p>Since people are still scrolling past the portfolio, I will include an "about me" section to highlight some of my professional and life experience and give readers a feel for my personality.</p>
+				<h3>Fourth Scroll-point/Skillz, yo</h3>
 					<p>Next, I will list the software, tool sets, and other skills I'm proficient in or have experience using. It's possible I'll use a visual representation of my skills in the form of a graph or chart, and maybe add links to some certificates from Lynda, etc to show my dedication to learning.</p>
 				<h3>Final Scroll-point/Contact Me!</h3>
 					<p>The next thing I'll need include will be my contact information, including my email address, links to my linkedIn and Instagram profiles, and a link to my downloadable resume. At the end of the page will be an embedded contact form so potential employers can reach me directly through the site.</p>
-					<p>I'm considering adding some reviews from happy customers, co-workers, instructors, or employers saying how great of a person I am to work with so that hiring authorities on the fence might be swayed to get in touch - we shall see what I come up with.</p>
-
 		<h2>Wireframes</h2>
 
 		<p>Coming soon...</p>
