37b295cf9f46584fc5d5d8e24f01d2d9b0cbfb78
Noel Cothren <noelcothren@gmail.com>
2017-02-02 09:31:24 -0700

    adding some images of wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,7 +3,30 @@
 	<head>
 		<meta charset="utf-8">
 		<title>PWP Milestone 2</title>
+		<style>
+			div.img {
+				margin: 5px;
+				border: 1px solid #ccc;
+				float: left;
+				width: 180px;
+			}
+
+			div.img:hover {
+				border: 1px solid #777;
+			}
+
+			div.img img {
+				width: 100%;
+				height: auto;
+			}
+
+			div.desc {
+				padding: 15px;
+				text-align: center;
+			}
+		</style>
 	</head>
+</html>
 	<body>
 		<h1>Personal Website Project: Milestone 2 Documentation</h1>
 			<header>
@@ -12,27 +35,49 @@
 			</header>
 				<div class="first">
 					<h3>Opening Page</h3>
-					<p>The first thing I want people to see is a well-thought-out (read: aesthetically pleasing and incredibly responsive) image frame with either my picture or a video (exact content tbd), overlaid with a "hello/welcome" message and my name. I would like it to be simple yet intriguing enough to make visitors scroll down. I am considering including a favorite quote.</p>
+					<p>The first thing I want people to see is a well-thought-out (read: aesthetically pleasing and incredibly responsive) image frame with either my picture or a video (exact content tbd), overlaid with a "hello/welcome" message and my name. I would like it to be simple yet intriguing enough to make visitors scroll down. I am considering including a favorite quote. I decided to lay out (in a fibonacci-inspired style) small thumbnails with links to each section of my portfolio, for ease of navigation.</p>
 				</div>
 				<div class="second">
 					<h3>Portfolio</h3>
-					<p>Since my purpose is to showcase my abilities as a Web Developer, the very next thing on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I think a gallery of images with clickable links will be ideal.</p>
+					<p>Since my purpose is to showcase my abilities as a Web Developer, the very next thing on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I think a gallery of images with clickable links will be ideal. To keep with the layout consistency, I will place images or colors in the spaces in my layout that are not filled with actual development projects. </p>
 				</div>
 				<div class="third">
 					<h3>Third Scroll-point/About Me</h3>
-					<p>Since people are still scrolling past the portfolio, I will include an "about me" section to highlight some of my professional and life experience and give readers a feel for my personality.</p>
+					<p>Since people are still scrolling past the portfolio, I will include an "about me" section to highlight some of my professional and life experience and give readers a feel for my personality. The background will be a photo of Albuquerque. </p>
 				</div>
 				<div class="fourth">
 					<h3>Fourth Scroll-point/Skillz, yo</h3>
-					<p>Next, I will list the software, tool sets, and other skills I'm proficient in or have experience using, and probably add links to some certificates from Lynda, etc to show my dedication to learning.</p>
+					<p>Next, I will display a visual of the software, tool sets, and other skills I'm proficient in or have experience using, and probably add links to some certificates from Lynda, etc to show my dedication to learning.</p>
 				</div>
 				<div class="final">
 					<h3>Final Scroll-point/Contact Me!</h3>
-					<p>The next thing I'll need include will be my contact information, including my email address, links to my linkedIn and Instagram profiles, and a link to my downloadable resume. At the end of the page will be an embedded contact form so potential employers can reach me directly through the site.</p>
+					<p>The next thing I'll include will be a footer with my contact information, including my email address, links to my linkedIn, GitHub, and Instagram profiles, and a link to my downloadable resume. At the end of the page will be an embedded contact form so potential employers can reach me directly through the site.</p>
 				</div>
 		<div class="wireframes">
 			<h2>Wireframes</h2>
-			<p>Coming soon...</p>
+			<h3>Desktop</h3>
+				<div class="img">
+						<img src="../images/desktop_1_welcome.tif" alt="Welcome">
+					</a>
+					<div class="desc">Add a description of the image here</div>
+				</div>
+
+				<div class="img">
+						<img src="../images/desktop_2_portfolio.tif" alt="Portfolio">
+					</a>
+					<div class="desc">Add a description of the image here</div>
+				</div>
+
+				<div class="img">
+						<img src="../images/desktop_3_about_me.tif" alt="About Me">
+					</a>
+					<div class="desc">Add a description of the image here</div>
+				</div>
+			<div class="img">
+					<img src="../images/desktop_4_Skillz.tif" alt="Skills">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
 		</div>
 	</body>
 
