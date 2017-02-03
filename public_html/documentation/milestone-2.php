46345533904c525b2162c2b567d41ec6b188fe3a
Noel Cothren <noelcothren@gmail.com>
2017-02-02 19:54:54 -0700

    chaged some wording

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,26 +13,23 @@
 			</header>
 				<div class="first">
 					<h3>Opening Page</h3>
-					<p>The first thing I want people to see is a well-thought-out (read: aesthetically pleasing and incredibly responsive) image frame with either my picture or a video (exact content tbd), overlaid with a "hello/welcome" message and my name. I would like it to be simple yet intriguing enough to make visitors scroll down. I am considering including a favorite quote.</p>
+					<p>The first thing I want people to see is a well-thought-out (read: aesthetically pleasing and incredibly responsive) image frame with either my picture or a video (exact content tbd), overlaid with a "hello/welcome" message and my name. I would like it to be simple yet intriguing enough to make visitors scroll down. I am considering including a favorite quote. I'm going to do a fibonacci-inspired layout with all navigation links on the first page, as well as at the top.</p>
 				</div>
 				<div class="second">
 					<h3>Portfolio</h3>
-					<p>Since my purpose is to showcase my abilities as a Web Developer, the very next thing on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I think a layout of images with clickable links will be ideal. </p>
+					<p>Since my purpose is to showcase my abilities as a Web Developer, the very next thing on this website will be my development portfolio, hopefully filled with lots of interesting and useful sites and apps I've created. I think a layout of images with clickable links will be ideal. The layout will keep the fibonnaci theme but I will need to fill in space with colors or images to make up for my current lack of portfolio items. </p>
 				</div>
 				<div class="third">
 					<h3>Third Scroll-point/About Me</h3>
-					<p>Since people are still scrolling past the portfolio, I will include an "about me" section to highlight some of my professional and life experience and give readers a feel for my personality.</p>
+					<p>Since people are still scrolling past the portfolio, I will include an "about me" section to highlight some of my professional and life experience and give readers a feel for my personality. The background image will be a photo I took of Albuquerque.</p>
 				</div>
 				<div class="fourth">
 					<h3>Fourth Scroll-point/Skillz, yo</h3>
-					<p>Next, I will display a visual of the software, tool sets, and other skills I'm proficient in or have experience using, and probably add links to some certificates from Lynda, etc to show my dedication to learning.</p>
+					<p>Next, I will display images or logos for the software, tool sets, and other skills I'm proficient in or have experience using, and probably add links to some certificates from Lynda, etc to show my dedication to learning.</p>
 				</div>
 				<div class="final">
 					<h3>Final Scroll-point/Contact Me!</h3>
 					<p>The next thing I'll include will be a footer with my contact information (showing under the skills section), including my email address, links to my linkedIn, GitHub, and Instagram profiles, and a link to my downloadable resume. After scrolling past the footer, at the end of the page will be an embedded contact form so potential employers can reach me directly through the site.</p>
 				</div>
-		<h2>Wireframes</h2>
-		<h3>Desktop</h3>
-		<h3>iPhone</h3>
 	</body>
 </html>
