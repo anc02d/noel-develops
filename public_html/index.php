4649a07ba4fb19638f54a35b979ce6dc9eea55dd
Noel Cothren <noelcothren@gmail.com>
2017-02-19 09:40:49 -0700

    two new files to "try" to get my layout straight

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,90 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<!--Bootstrap Stylesheet from CDN-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+		<!--My Custom CSS stylesheet @author Noel Cothren ---->
+		<link rel="stylesheet" href="styles/frontend-styles.css" />
+		<!--Bootstrap jQuery and Javascript--->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Noel Develops</title>
+	</head>
+
+	<body>
+
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<ul class="navbar-left">
+				<li>welcome</li>
+				<li>portfolio</li>
+				<li>skills</li>
+			</ul>
+
+				<a class="navbar-brand" id="logo" href="#">noel < <span class="fa fa-magic"></span> > develops</span></a>
+
+			<ul class="navbar-right">
+				<li>about</li>
+				<li>links</li>
+				<li>contact</li>
+			</ul>
+
+		</nav>
+		<container class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-xs-12 col-md-5" id="welcome">
+				<div class="content">
+					<div class="table">
+						<div class="table-cell">
+							<p>Welcome!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-1" id="about">
+				<div class="content">
+					<div class="table">
+						<div class="table-cell">
+							<p>About</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-1" id="skills">
+				<div class="content">
+					<div class="table">
+						<div class="table-cell">
+							<p>Skills</p>
+						</div>
+					</div>
+				</div>
+			</div>
+				<div class="col-xs-12 col-md-2" id="contact">
+				<div class="content">
+					<div class="table">
+						<div class="table-cell">
+							<p>Contact Me</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-xs-6 col-md-3" id="portfolio">
+				<div class="content">
+					<div class="table">
+						<div class="table-cell">
+							<p>Work & Play</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-1"></div>
+		</div>
+		</container>
+	</body>
+</html>
