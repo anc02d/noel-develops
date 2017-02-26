8f7478de174f9325e8aa63c22542cc01934221c3
Noel Cothren <noelcothren@gmail.com>
2017-02-26 16:59:13 -0700

    uhhh probably shouldve pushed a long time ago lots of changes

--- public_html/index-trial.php
+++ public_html/index-trial.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8" />
+		<meta charset="UTF-8"/>
 		<!--Bootstrap Stylesheet from CDN-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!-- Font Awesome -->
-		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+				rel="stylesheet"/>
 		<!--My Custom CSS stylesheet @author Noel Cothren ---->
-		<link rel="stylesheet" href="styles/style-trial.css" />
+		<link rel="stylesheet" href="styles/style-trial.css"/>
 		<!--Bootstrap jQuery and Javascript--->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<title>Noel Develops</title>
 	</head>
@@ -18,21 +22,14 @@
 	<body>
 		<div class="container-fluid" id="welcome-page">
 			<div class="row">
-				<div class="col-md-2"></div>
-					<div class="col-md-8"></div>
-					<div class="col-md-5">
-						<div class="row">
-							<div class="col-md-6">
-								<div class="row">
-									<div class="col-md-12"></div>
-									<div class="col-md-12"></div>
-								</div>
-							</div>
-							<div class="col-md-6"></div>
-						</div>
-					</div>
-				<div class="col-md-2"></div>
+				<div class="col-md-8">
+					<img src="images/desert-bkgd.jpg" alt="desert background" class="img-responsive">
+					<h2>Hi. I'm Noel.</h2>
+					<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle, shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle tortizzle. Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus dang. </p>
+				</div>
+				<div class="col-md-4">
+
+				</div>
 			</div>
-		</div>
 	</body>
 </html>
--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8" />
+		<meta charset="UTF-8"/>
 		<!--Bootstrap Stylesheet from CDN-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!-- Font Awesome -->
-		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+				rel="stylesheet"/>
 		<!--My Custom CSS stylesheet @author Noel Cothren ---->
-		<link rel="stylesheet" href="styles/frontend-styles.css" />
+		<link rel="stylesheet" href="styles/frontend-styles.css"/>
 		<!--Bootstrap jQuery and Javascript--->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 		<title>Noel Develops</title>
@@ -17,104 +19,118 @@
 
 	<body>
 		<div class="container">
-		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" id="logo" href="#">NOEL < |
-<!--					<span class="fa fa-magic"></span> -->
-					> DEVELOPS</span></a>
-			</div>
+			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" id="logo" href="#welcome-section">Noel <|> Develops</a>
+				</div>
 
-			<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li class="navbar-left"><a href="#">Home</a></li>
-					<li class="navbar-left"><a href="#about">About</a></li>
-					<li class="navbar-left"><a href="#skills">Skills</a></li>
-					<li class="navbar-right"><a href="#portfolio">Portfolio</a></li>
-					<li class="navbar-right"><a href="#links">Links</a></li>
-					<li class="navbar-right"><a href="#contact">Contact</a></li>
-				</ul>
-			</div>
-		</nav>
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="navbar-left"><a href="#welcome-section">Home</a></li>
+						<li class="navbar-left"><a href="#about-section">About</a></li>
+						<li class="navbar-left"><a href="#skills-section">Skills</a></li>
+						<li class="navbar-right"><a href="#portfolio-section">Portfolio</a></li>
+						<li class="navbar-right"><a href="#skills-section">Links</a></li>
+						<li class="navbar-right"><a href="#contact-section">Contact</a></li>
+					</ul>
+				</div>
+			</nav>
 		</div>
+<div class="container-fluid reveal" style="margin-bottom: 498px;">
+		<div class="container-fluid" id="welcome-section">
+			<div class="row">
+				<div class="col-xs-12 col-md-7 col-md-offset-1" id="welcome">
+					<div class="content">
+						<div class="table">
+							<div class="table-cell">
+								<p>Hello, world!</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-4" id="portfolio-home-div">
+					<div class="content">
+						<div class="table">
+							<div class="table-cell">
+								<p>Work & Play</p>
+							</div>
+						</div>
+					</div>
+				</div>
+					<div class="col-xs-12 col-md-4" id="contact">
+						<div class="content">
+							<div class="table">
+								<div class="table-cell">
+									<p>Contact Me</p>
+								</div>
+							</div>
+						</div>
+					</div>
+			</div> <!--end main welcome page row-->
+		</div><!--end welcome page section container-->
+		<div class="container-fluid" id="portfolio-section">
+			<div class="row">
+				<div class="col-xs-12 col-md-4 col-md-offset-1 portfolio-img-div"></div>
+				<div class="col-xs-12 col-md-4" id="portfolio-text">
+					<h2>WORK & PLAY</h2>
+					<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle, 	shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle tortizzle.	Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus dang. </p>
+					<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz sizzle check it out. Uhuh ... yih! </p>
+					<p>Rizzle a bow wow wow crazy mi dang ass. Curabitur yo gangster ipsizzle. Fo shizzle habitant morbirizzle senectus et netizzle izzle we gonna chung fames izzle brizzle pot. In est. Curabitizzle elementum. Ut eros felis, dope sure, pimpin' izzle, gangster pulvinizzle, i'm in the shizzle. Nulla sagittizzle gravida velit.</p>
+				</div>
+				<div class="col-xs-12 col-md-4 col-md-offset-1 portfolio-img-div"></div>
+			</div>
+		</div><!--end portfolio container -->
+		<div class="container-fluid" id="about-section">
+			<div class="row" id="about-img">
+				<div class="col-md-4 col-md-offset-1" id="about-text">
+					<br/><br/>
+						<h2>Hi. I'm Noel.</h2>
+						<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle, shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle tortizzle. Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus dang. </p>
+						<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz 	sizzle check it out. Uhuh ... yih! </p>
+					</div>
+			</div>
+		</div> <!--end #about-section container -->
+		<div class="row" id="skills-section">
+			<div class="col-md-2"></div>
+			<div class="col-md-2"></div>
+			<div class="col-md-2"></div>
+			<div class="col-md-2"></div>
+			<div class="col-md-2"></div>
+			<div class="col-md-2"></div>
+		</div>
+</div>
+		<footer>
+			<div class="row" id="contact-section">
+				<h2>Let's work together!</h2>
+				<p>Send me a message to get in touch.</p> <br/>
+				<div class="col-md-8 col-md-offset-2">
+					<div class="form-group">
+						<label for="formInputEmail">Email address
+						</label>
+						<input type="email" class="form-control" id="formInputEmail" placeholder="myemail@mysite.com">
+					</div>
+					<div class="form-group">
+						<label for="formInputName">Name &/or Company
+						</label>
+						<input type="text" id="formInputName" class="form-control" maxlength="64" placeholder="Your name and/or the name of your business">
+					</div>
+					<div class="form-group form-horizontal-well">
+						<label for="formInputMessage">Message
+						</label>
+						<input type="text" maxlength="250" class="form-control" id="formInputMessage" placeholder="How can I help you?">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</div>
+				<div class="col-md-4">
 
-		<div class="container-fluid" id="welcome-page">
-		<div class="row">
-			<div class="col-md-8" id="welcome">
-				<div class="content">
-					<div class="table">
-						<div class="table-cell">
-							<p>Welcome!</p>
-						</div>
-					</div>
 				</div>
 			</div>
-			<div class="col-md-4" id="portfolio-home-div">
-				<div class="content">
-					<div class="table">
-						<div class="table-cell">
-							<p>Work & Play</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row"><!--contains about, skills, contact boxes-->
-				<div class="col-md-12" id="about">
-					<div class="content">
-						<div class="table">
-							<div class="table-cell">
-								<p>About</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6" id="contact">
-					<div class="content">
-						<div class="table">
-							<div class="table-cell">
-								<p>Contact Me</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-12" id="skills">
-					<div class="content">
-						<div class="table">
-							<div class="table-cell">
-								<p>Skills</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div> <!--end row inside of col-md-5 -->
-		</div> <!--end main welcome page row-->
-	</div><!--end welcome page section container-->
-	<div class="container-fluid" id="portfolio-container">
-		<div class="row">
-			<div class="col-md-4 col-md-offset-1 portfolio-img-div"></div>
-			<div class="col-md-4" id="portfolio-text">
-				<h2>WORK & PLAY</h2>
-				<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle, shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle tortizzle. Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus dang. </p>
-				<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz sizzle check it out. Uhuh ... yih! </p>
-				<p>Rizzle a bow wow wow crazy mi dang ass. Curabitur yo gangster ipsizzle. Fo shizzle habitant morbi rizzle senectus et netizzle izzle we gonna chung fames izzle brizzle pot. In est. Curabitizzle elementum. Ut eros felis, dope sure, pimpin' izzle, gangster pulvinizzle, i'm in the shizzle. Nulla sagittizzle gravida velit.</p>
-			</div>
-			<div class="col-md-4 col-md-offset-1 portfolio-img-div"></div>
-		</div>
-	</div><!--end portfolio container -->
-	<div class="container-fluid" id="about-section">
-		<div class="row" id="about-img">
-			<div class="col-md-4 col-md-offset-2" id="about-text">
-				<h2>Nice to Meet You...</h2>
-				<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle, shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle tortizzle. Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus dang. </p>
-				<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz sizzle check it out. Uhuh ... yih! </p>
-				<p>Rizzle a bow wow wow crazy mi dang ass. Curabitur yo gangster ipsizzle. Fo shizzle habitant morbi rizzle senectus et netizzle izzle we gonna chung fames izzle brizzle pot. In est. Curabitizzle elementum. Ut eros felis, dope sure, pimpin' izzle, gangster pulvinizzle, i'm in the shizzle. Nulla sagittizzle gravida velit.</p>
-			</div>
-			<div class="col-md-6"></div>
-		</div>
-		</div>
+		</footer>
+
 	</body>
 </html>
