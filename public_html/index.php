1d711e7589b3b973b54b55dca75e90da4758d15b
Noel Cothren <noelcothren@gmail.com>
2017-03-02 20:53:15 -0700

    got my contact form to show correctly on smaller screens, made sure welcome image and text re-size properly

--- public_html/index.php
+++ public_html/index.php
@@ -19,12 +19,17 @@
 		<!--jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 		<!--Bootstrap Javascript--->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<!-- jQuery Form, Validate, Additional Methods -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"></script>
 
 		<!-- My JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
@@ -59,93 +64,107 @@
 				</div>
 			</nav>
 		</div>
-<div class="container-fluid reveal">
-		<div id="welcome-section">
-			<div class="row">
-				<div class="col-xs-12 col-md-8">
-					<div class="welcome-box">
-						<div class="homepage-box-text">
-								<h3>Hey there, this site is in progress!<br /> Check back soon for a better experience!</h3>
-						</div>
-					</div>
-				</div>
-				<div class="col-xs-12 col-md-4" id="portfolio-home-div">
-					<div class="row">
-						<div class="col-xs-12 col-md-12">
-							<div class="portfolio-box">
-								<div class="homepage-box-text">
-									<h3>Portfolio</h3>
-								</div>
+		<div class="container-fluid reveal">
+			<div id="welcome-section">
+				<div class="row">
+					<div class="col-xs-12 col-md-8">
+						<div class="welcome-box">
+							<div class="homepage-box-text">
+								<h3>Hey there, this site is in progress!<br/> Check back soon for a better experience!</h3>
 							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-xs-12 col-md-6">
-							<div class="row">
-								<div class="col-xs-12 col-md-12">
-									<div class="about-box">
-										<div class="homepage-box-text">
-											<h3>About</h3>
-										</div>
-									</div>
-								</div>
-								<div class="col-xs-12 col-md-12">
-									<div class="skills-box">
-										<div class="homepage-box-text">
-											<h3>Skills</h3>
-										</div>
+					<div class="col-xs-12 col-md-4" id="portfolio-home-div">
+						<div class="row">
+							<div class="col-xs-12 col-md-12">
+								<div class="portfolio-box">
+									<div class="homepage-box-text">
+										<h3>Portfolio</h3>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div class="col-xs-12 col-md-6">
-							<div class="contact-box">
-								<div class="homepage-box-text">
-									<h3>Contact</h3>
+						<div class="row">
+							<div class="col-xs-12 col-md-6">
+								<div class="row">
+									<div class="col-xs-12 col-md-12">
+										<div class="about-box">
+											<div class="homepage-box-text">
+												<h3>About</h3>
+											</div>
+										</div>
+									</div>
+									<div class="col-xs-12 col-md-12">
+										<div class="skills-box">
+											<div class="homepage-box-text">
+												<h3>Skills</h3>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-md-6">
+								<div class="contact-box">
+									<div class="homepage-box-text">
+										<h3>Contact</h3>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+				</div> <!--end main welcome page row-->
+			</div><!--end welcome page section container-->
+			<div id="portfolio-section">
+				<div class="row">
+					<div class="col-xs-12 col-md-4 portfolio-img-div"></div>
+					<div class="col-xs-12 col-md-4" id="portfolio-text">
+						<h2>WORK & PLAY</h2>
+						<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle,
+							shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle
+							tortizzle. Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle
+							izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus
+							dang. </p>
+						<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle
+							stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz
+							sizzle check it out. Uhuh ... yih! </p>
+						<p>Rizzle a bow wow wow crazy mi dang ass. Curabitur yo gangster ipsizzle. Fo shizzle habitant
+							morbirizzle senectus et netizzle izzle we gonna chung fames izzle brizzle pot. In est. Curabitizzle
+							elementum. Ut eros felis, dope sure, pimpin' izzle, gangster pulvinizzle, i'm in the shizzle. Nulla
+							sagittizzle gravida velit.</p>
+					</div>
+					<div class="col-xs-12 col-md-4 portfolio-img-div"></div>
 				</div>
-			</div> <!--end main welcome page row-->
-		</div><!--end welcome page section container-->
-		<div id="portfolio-section">
-			<div class="row">
-				<div class="col-xs-12 col-md-4 portfolio-img-div"></div>
-				<div class="col-xs-12 col-md-4" id="portfolio-text">
-					<h2>WORK & PLAY</h2>
-					<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle, shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle tortizzle.	Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus dang. </p>
-					<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz sizzle check it out. Uhuh ... yih! </p>
-					<p>Rizzle a bow wow wow crazy mi dang ass. Curabitur yo gangster ipsizzle. Fo shizzle habitant morbirizzle senectus et netizzle izzle we gonna chung fames izzle brizzle pot. In est. Curabitizzle elementum. Ut eros felis, dope sure, pimpin' izzle, gangster pulvinizzle, i'm in the shizzle. Nulla sagittizzle gravida velit.</p>
-				</div>
-				<div class="col-xs-12 col-md-4 portfolio-img-div"></div>
-			</div>
-		</div><!--end portfolio container -->
-		<div id="about-section">
-			<div class="row" id="about-img">
-				<div class="col-xs-12 col-md-4 col-md-offset-1" id="about-text">
-					<br/><br/>
+			</div><!--end portfolio container -->
+			<div id="about-section">
+				<div class="row" id="about-img">
+					<div class="col-xs-12 col-md-4 col-md-offset-1" id="about-text">
+						<br/><br/>
 						<h2>Hi. I'm Noel.</h2>
-						<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle, shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle tortizzle. Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus dang. </p>
-						<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz 	sizzle check it out. Uhuh ... yih! </p>
+						<p>Lorizzle sure dolizzle sit dang, you son of a bizzle adipiscing elit. Yippiyo tellivizzle velizzle,
+							shizznit volutpizzle, suscipizzle quis, sure vizzle, fo shizzle. Uhuh ... yih! tellivizzle
+							tortizzle. Sed eros. Shizzle my nizzle crocodizzle izzle its fo rizzle dapibus dawg tempizzle
+							izzle. Mauris pellentesque nibh et turpizzle. Vestibulum crunk . Pellentesque eleifend rhoncus
+							dang. </p>
+						<p>In fo shizzle fizzle dictumst. Get down get down dapibizzle. Curabitur tellus urna, pretizzle
+							stuff, mattizzle boofron, bling bling vitae, nunc. Break it down suscipit. Integizzle dang shiz
+							sizzle check it out. Uhuh ... yih! </p>
 					</div>
+				</div>
+			</div> <!--end #about-section container -->
+			<div class="row" id="skills-section">
+				<div class="col-xs-12 col-md-2"></div>
+				<div class="col-xs-12 col-md-2"></div>
+				<div class="col-xs-12 col-md-2"></div>
+				<div class="col-xs-12 col-md-2"></div>
+				<div class="col-xs-12 col-md-2"></div>
+				<div class="col-xs-12 col-md-2"></div>
 			</div>
-		</div> <!--end #about-section container -->
-		<div class="row" id="skills-section">
-			<div class="col-xs-12 col-md-2"></div>
-			<div class="col-xs-12 col-md-2"></div>
-			<div class="col-xs-12 col-md-2"></div>
-			<div class="col-xs-12 col-md-2"></div>
-			<div class="col-xs-12 col-md-2"></div>
-			<div class="col-xs-12 col-md-2"></div>
 		</div>
-</div>
-		<footer>
-			<div class="row" id="contact-section">
-				<h1>Let's work together!</h1>
-				<p>Send me a message to get in touch.</p> <br/>
-				<div class="col-xs-12 col-md-8 col-md-offset-2">
-					<form id="contact-form" action="php/mailer.php" method="post">
+		<div class="row" id="contact-section">
+			<h1>Let's work together!</h1>
+			<p>Send me a message to get in touch.</p> <br/>
+			<div class="col-xs-12 col-md-8 col-md-offset-2">
+				<form id="contact-form" action="php/mailer.php" method="post">
 					<div class="form-group">
 						<label for="formInputEmail">Email address
 						</label>
@@ -153,7 +172,8 @@
 					</div>
 					<div class="form-group">
 						<label for="formInputName">Name &/or Company</label>
-						<input type="text" id="name" name="name" class="form-control" maxlength="64" placeholder="Your name and/or the name of your business">
+						<input type="text" id="name" name="name" class="form-control" maxlength="64"
+								 placeholder="Your name and/or the name of your business">
 					</div>
 					<div class="form-group form-horizontal-well">
 						<label for="formInputMessage">
@@ -162,17 +182,16 @@
 						<textarea class="form-control" rows="5" id="message" name="message" placeholder="How can I help you?">
 						</textarea>
 					</div>
-						<div class="g-recaptcha" data-sitekey="6LdXbRcUAAAAAD8YqILaySEsGeARVv2DDCLkl3tE"></div>
+					<div class="g-recaptcha" data-sitekey="6LdXbRcUAAAAAD8YqILaySEsGeARVv2DDCLkl3tE"></div>
 					<button type="submit" class="btn btn-default">Submit</button>
 				</form>
-					<!--empty area for form error/success output-->
-					<div class="row">
-						<div class="col-xs-12">
-							<div id="output-area"></div>
-						</div>
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
 					</div>
+				</div>
 			</div>
-		</footer>
-
+		</div>
 	</body>
 </html>
